2c37ca4309b0eb320e375bc61aa650facecf8425
Arron <arron@acusense.ai>
2017-08-29 16:31:00 -0700

    checkpoint 1

--- _datmo/python_api_template.py
+++ _datmo/python_api_template.py
@@ -10,17 +10,18 @@ app = Flask(__name__)
 @app.route('/<func_name>', methods=['POST'])
 def api_root(func_name):
     for function in functions_list:
-        try:
-            json_req_data = request.get_json()
-            print json_req_data, "check"
-            if json_req_data:
-                res = function(json_req_data)
-        except Exception as e:
-            return jsonify({"error": "Something is wrong"+str(e)})
-        return jsonify({"result": res})
+        if function.__name__ == func_name:
+            try:
+                json_req_data = request.get_json()
+                if json_req_data:
+                    res = function(json_req_data)
+                else:
+                    return jsonify({"error": "error in receiving the json input"})
+            except Exception as e:
+                return jsonify({"error": "error while running the function"})
+            return jsonify({"result": res})
     output_string = 'function: %s not found' % func_name
     return jsonify({"error": output_string})
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
